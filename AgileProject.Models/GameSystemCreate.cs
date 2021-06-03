a3fadcec707d5f962f9abe51f89104bf2e108b54
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-03 16:29:20 -0400

    Added GameSystem Models

---
+++ AgileProject.Models/GameSystemCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileProject.Models
+{
+    public class GameSystemCreate
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Manufacturer { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
