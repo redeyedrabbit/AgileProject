2480b92619dafbc588126c189665914a604a70e3
Rachel Bellwood <rbellwood@gmail.com>
2021-06-03 16:43:54 -0400

    Genre files created

---
+++ AgileProject.Data/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileProject.Data
+{
+    // Rachel
+    public enum GenreType { Action = 1, Adventure, Fighting, Platform, Puzzle, Racing, Shooter, Simulation, Sports, Strategy, Misc }
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public GenreType TypeOfGenre { get; set; }
+    }
+}
---
+++ AgileProject/Controllers/GenreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AgileProject.Controllers
+{
+    public class GenreController : ApiController
+    {
+        // Get All Genres
+        public IHttpActionResult Get()
+        {
+            GenreService genreService = CreateGenreService();
+            var genres = genreService.GetGenre();
+            return Ok(genres);
+        }
+
+        // Get Genre by Game Title
+        public IHttpActionResult Get(int id)
+        {
+            GenreService genreService = CreateGenreService();
+            var genre = genreService.GetGenreByGameId(id);
+            return Ok(genre);
+        }
+    }
+}
